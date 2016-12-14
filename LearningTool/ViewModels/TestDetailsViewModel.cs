2d4ef5ecc7cd9105a8c8d70eecea741022a9a4bc
pausone <pausone@users.noreply.github.com>
2016-12-14 11:13:57 +0100

    Change path for site

---
+++ LearningTool/ViewModels/TestDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LearningTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningTool.ViewModels
+{
+    public class TestDetailsViewModel
+    {
+        public int TestId { get; set; }
+
+        public string TestSubject { get; set;}
+
+        public List<QuestionAndAnswer> Questions { get; set; }
+
+    }
+}
